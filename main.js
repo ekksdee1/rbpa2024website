50d5ef150dc8b4e57487ef989f56d6fd587a8282
ekksdee1 <112628685+ekksdee1@users.noreply.github.com>
2024-01-11 14:06:05 -0500

    e

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
-$("deli").on("click", function(){
-    
-})
+var pickup_button = document.getElementById("pick");
+var delivery_button = document.getElementById("deli");
+var pick_expand = document.getElementById("pick-expand");
+var deli_expand = document.getElementById("deli-expand");
+
+pickup_button.addEventListener("click", function(){
+    pick_expand.style.visibility = visible;
+});
+
+delivery_button.addEventListener("click", function(){
+    deli_expand.style.visibility = visible;
+});
