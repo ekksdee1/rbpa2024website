58d6840861cde70522996a59e90f0eb280abcbcb
804023 <804023@sylvaniastudents.org>
2024-01-24 14:19:04 -0500

    view changes Copied over the temporary javascript from menu.html to here.

---
+++ menu.js
@@ -0,0 +1,188 @@
+$(document).ready(function() {
+    $('.hidden').hide();
+    $('.starters').on('click', function() {
+        $('.food-categories').hide();
+        $('.starters-items').show();
+    });
+    $('.pretzel-bites-more').on('click', function() {
+        $('.pretzel-bites-info').show();
+        $('.pretzel-bites-item').hide();
+    });
+    $('.pretzel-bites-less').on('click', function() {
+        $('.pretzel-bites-info').hide();
+        $('.pretzel-bites-item').show();
+    });
+    $('.fried-pickles-more').on('click', function() {
+        $('.fried-pickles-info').show();
+        $('.fried-pickles-item').hide();
+    });
+    $('.fried-pickles-less').on('click', function() {
+        $('.fried-pickles-info').hide();
+        $('.fried-pickles-item').show();
+    });
+    $('.cheese-curds-more').on('click', function() {
+        $('.cheese-curds-info').show();
+        $('.cheese-curds-item').hide();
+    });
+    $('.cheese-curds-less').on('click', function() {
+        $('.cheese-curds-info').hide();
+        $('.cheese-curds-item').show();
+    });
+    $('.cheese-bread-more').on('click', function() {
+        $('.cheese-bread-info').show();
+        $('.cheese-bread-item').hide();
+    });
+    $('.cheese-bread-less').on('click', function() {
+        $('.cheese-bread-info').hide();
+        $('.cheese-bread-item').show();
+    });
+    $('.mozzarella-sticks-more').on('click', function() {
+        $('.mozzarella-sticks-info').show();
+        $('.mozzarella-sticks-item').hide();
+    });
+    $('.mozzarella-sticks-less').on('click', function() {
+        $('.mozzarella-sticks-info').hide();
+        $('.mozzarella-sticks-item').show();
+    });
+    $('.salads').on('click', function() {
+        $('.food-categories').hide();
+        $('.salad-items').show();
+    });
+    $('.caesar-salad-more').on('click', function() {
+        $('.caesar-salad-info').show();
+        $('.caesar-salad-item').hide();
+    });
+    $('.caesar-salad-less').on('click', function() {
+        $('.caesar-salad-info').hide();
+        $('.caesar-salad-item').show();
+    });
+    $('.house-salad-more').on('click', function() {
+        $('.house-salad-info').show();
+        $('.house-salad-item').hide();
+    });
+    $('.house-salad-less').on('click', function() {
+        $('.house-salad-info').hide();
+        $('.house-salad-item').show();
+    });
+    $('.grilled-chicken-salad-more').on('click', function() {
+        $('.grilled-chicken-salad-info').show();
+        $('.grilled-chicken-salad-item').hide();
+    });
+    $('.grilled-chicken-salad-less').on('click', function() {
+        $('.grilled-chicken-salad-info').hide();
+        $('.grilled-chicken-salad-item').show();
+    });
+    $('.seafood').on('click', function() {
+        $('.food-categories').hide();
+        $('.seafood-items').show();
+    });
+    $('.fish-n-chips-more').on('click', function() {
+        $('.fish-n-chips-info').show();
+        $('.fish-n-chips-item').hide();
+    });
+    $('.fish-n-chips-less').on('click', function() {
+        $('.fish-n-chips-info').hide();
+        $('.fish-n-chips-item').show();
+    });
+    $('.salads').on('click', function() {
+        $('.food-categories').hide();
+        $('.salad-items').show();
+    });
+    $('.caesar-salad-more').on('click', function() {
+        $('.caesar-salad-info').show();
+        $('.caesar-salad-item').hide();
+    });
+    $('.caesar-salad-less').on('click', function() {
+        $('.caesar-salad-info').hide();
+        $('.caesar-salad-item').show();
+    });
+    $('.house-salad-more').on('click', function() {
+        $('.house-salad-info').show();
+        $('.house-salad-item').hide();
+    });
+    $('.house-salad-less').on('click', function() {
+        $('.house-salad-info').hide();
+        $('.house-salad-item').show();
+    });
+    $('.grilled-chicken-salad-more').on('click', function() {
+        $('.grilled-chicken-salad-info').show();
+        $('.grilled-chicken-salad-item').hide();
+    });
+    $('.grilled-chicken-salad-less').on('click', function() {
+        $('.grilled-chicken-salad-info').hide();
+        $('.grilled-chicken-salad-item').show();
+    });
+    $('.seafood').on('click', function() {
+        $('.food-categories').hide();
+        $('.seafood-items').show();
+    });
+    $('.fish-n-chips-more').on('click', function() {
+        $('.fish-n-chips-info').show();
+        $('.fish-n-chips-item').hide();
+    });
+    $('.fish-n-chips-less').on('click', function() {
+        $('.fish-n-chips-info').hide();
+        $('.fish-n-chips-item').show();
+    });
+    $('.grilled-salmon-more').on('click', function() {
+        $('.grilled-salmon-info').show();
+        $('.grilled-salmon-item').hide();
+    });
+    $('.grilled-salmon-less').on('click', function() {
+        $('.grilled-salmon-info').hide();
+        $('.grilled-salmon-item').show();
+    });
+    $('.shrimp-scampi-more').on('click', function() {
+        $('.shrimp-scampi-info').show();
+        $('.shrimp-scampi-item').hide();
+    });
+    $('.shrimp-scampi-less').on('click', function() {
+        $('.shrimp-scampi-info').hide();
+        $('.shrimp-scampi-item').show();
+    });
+    $('.burgers-and-sandwiches').on('click', function() {
+        $('.food-categories').hide();
+        $('.burgers-and-sandwiches-items').show();
+    });
+    $('.cheeseburger-more').on('click', function() {
+        $('.cheeseburger-info').show();
+        $('.cheeseburger-item').hide();
+    });
+    $('.cheeseburger-less').on('click', function() {
+        $('.cheeseburger-info').hide();
+        $('.cheeseburger-item').show();
+    });
+    $('.philly-steak-more').on('click', function() {
+        $('.philly-steak-info').show();
+        $('.philly-steak-item').hide();
+    });
+    $('.philly-steak-less').on('click', function() {
+        $('.philly-steak-info').hide();
+        $('.philly-steak-item').show();
+    });
+    $('.chicken-philly-more').on('click', function() {
+        $('.chicken-philly-info').show();
+        $('.chicken-philly-item').hide();
+    });
+    $('.chicken-philly-less').on('click', function() {
+        $('.chicken-philly-info').hide();
+        $('.chicken-philly-item').show();
+    });
+});
+/*
+Templates
+---------
+    $('.').on('click', function() {
+        $('.food-categories').hide();
+        $('.-items').show();
+    });
+    ------
+    $('.-more').on('click', function() {
+        $('.-info').show();
+        $('.-item').hide();
+    });
+    $('.-less').on('click', function() {
+        $('.-info').hide();
+        $('.-item').show();
+    });
+*/
